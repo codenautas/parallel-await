3bd6277e715bf3bf60f6038e4f69e3c9a80218d1
Emilio Platzer <emilioplatzer@gmail.com>
2019-07-14 17:38:08 -0300

    2 implementaciones de parallel-await

--- src/parallel-await.ts
+++ src/parallel-await.ts
@@ -7,6 +7,46 @@ function isPlainObject(x:any):boolean{
 export type Resolved<T> = T extends Promise<infer U> ? U : T;
 
 export async function parallel<T>(objectWithUnresolvedPromises:T):Promise<{ [P in keyof T]: Resolved<T[P]> } > {
+    return new Promise(function(resolve, reject){
+        var resolved:{[P in keyof T]: Resolved<T[P]>};
+        // @ts-ignore
+        resolved = {};
+        var countPending = 0;
+        var allLauncheds = false;
+        for(var attr in objectWithUnresolvedPromises){
+            (function(attr:keyof T){
+                countPending++;            
+                Promise.resolve(objectWithUnresolvedPromises[attr]).then(function(result){
+                    countPending--;
+                    // @ts-ignore
+                    resolved[attr]=result;
+                    if(allLauncheds && !countPending){
+                        resolve(resolved)
+                    }
+                },reject);
+            })(attr);
+        }
+        allLauncheds=true;
+        if(!countPending){
+            resolve(resolved)
+        }
+    });
+}
+
+export async function parallelNotAsSoonAsPosible<T>(objectWithUnresolvedPromises:T):Promise<{ [P in keyof T]: Resolved<T[P]> } > {
+    var resolved:{[P in keyof T]: Resolved<T[P]>};
+    // @ts-ignore
+    resolved = {};
+    for(var attr in objectWithUnresolvedPromises){
+        // @ts-ignore
+        resolved[attr] = await objectWithUnresolvedPromises[attr];
+    }
+    return resolved;
+}
+
+parallelNotAsSoonAsPosible.notAsSoonAsPosible=true;
+
+export async function parallelByPromiseAll<T>(objectWithUnresolvedPromises:T):Promise<{ [P in keyof T]: Resolved<T[P]> } > {
     var resolved:{[P in keyof T]: Resolved<T[P]>};
     // @ts-ignore
     resolved = {};
--- test/test.ts
+++ test/test.ts
@@ -5,7 +5,7 @@ const SPEED = 100;
 import {sleep} from 'best-globals';
 import {showAndThrow} from 'discrepances';
 
-import {parallel} from '../lib/parallel-await';
+import * as parallelModule from '../lib/parallel-await';
 
 async function slow<T>(target:string[], message:T, start:number, speed?:number):Promise<{r:T}>{
     await sleep(start);
@@ -21,44 +21,80 @@ async function slow<T>(target:string[], message:T, start:number, speed?:number):
     return {r:message};
 }
 
-describe('parallel-await', function(){
-    it("await all on all promises", async function(){
-        var target:string[] = []
-        var x = {
-            x1: slow(target, true , 10),
-            x2: slow(target, 123  , 30),
-            x3: slow(target, 'abc', 50),
-        }
-        var y = await parallel(x);
-        showAndThrow(y, {x1:{r:true}, x2:{r:123}, x3:{r:'abc'}});
-        showAndThrow(target.join(''), 't1ar2bu3ce');
-    })
-    it("await all on some promises and some not", async function(){
-        var target:string[] = []
-        var x = {
-            x1: slow(target, true , 10),
-            x2: 123,
-            x3: slow(target, 'abc', 50),
-        }
-        var y = await parallel(x);
-        showAndThrow(y, {x1:{r:true}, x2:123, x3:{r:'abc'}});
-        showAndThrow(target.join(''), 'tarbuce');
-    })
-    it("catch await as soon as posible", async function(){
-        this.timeout(4000);
-        var target:string[] = []
-        var x = {
-            x1: slow(target, true , 10, 500),
-            x2: 123,
-            x3: slow(target, 'a.c', 50, 500),
-        }
-        try{
+['parallel','parallelNotAsSoonAsPosible','parallelByPromiseAll'].forEach(function(funName){
+    var parallel = parallelModule[funName];
+    if(parallel.notAsSoonAsPosible){
+        // return ;
+    }
+    describe('parallel-await using '+funName, function(){
+        it("await all on all promises", async function(){
+            var target:string[] = []
+            var x = {
+                x1: slow(target, true , 10),
+                x2: slow(target, 123  , 30),
+                x3: slow(target, 'abc', 50),
+            }
             var y = await parallel(x);
-            throw new Error('must fail waiting');
-        }catch(err){
-            showAndThrow(err.message, 'good fail')
-            showAndThrow(target.join(''), 'tar');
-            showAndThrow(y, undefined);
-        }
-    })
-});
+            showAndThrow(y, {x1:{r:true}, x2:{r:123}, x3:{r:'abc'}});
+            showAndThrow(target.join(''), 't1ar2bu3ce');
+        })
+        it("await all on some promises and some not", async function(){
+            var target:string[] = []
+            var x = {
+                x1: slow(target, true , 10),
+                x2: 123,
+                x3: slow(target, 'abc', 50),
+            }
+            var y = await parallel(x);
+            showAndThrow(y, {x1:{r:true}, x2:123, x3:{r:'abc'}});
+            showAndThrow(target.join(''), 'tarbuce');
+        })
+        it("await all on not promises", async function(){
+            var x = {
+                x1: true,
+                x2: 123,
+                x3: 'abc',
+            }
+            var y = await parallel(x);
+            showAndThrow(y, {x1:true, x2:123, x3:'abc'});
+        })
+        it("catch await as soon as posible", async function(){
+            this.timeout(4000);
+            var target:string[] = []
+            var x = {
+                x1: slow(target, true , 10, 500),
+                x2: 123,
+                x3: slow(target, 'a.c', 50, 500),
+            }
+            try{
+                var y = await parallel(x);
+                throw new Error('must fail waiting');
+            }catch(err){
+                showAndThrow(err.message, 'good fail')
+                showAndThrow(y, undefined);
+                if(!parallel.notAsSoonAsPosible){
+                    showAndThrow(target.join(''), 'tar');
+                }
+            }
+        })
+        it("catch await as soon as posible with two errors", async function(){
+            this.timeout(4000);
+            var target:string[] = []
+            var x = {
+                x1: slow(target,'true.',10, 500),
+                x2: 123,
+                x3: slow(target, 'a.c', 50, 500),
+            }
+            try{
+                var y = await parallel(x);
+                throw new Error('must fail waiting');
+            }catch(err){
+                showAndThrow(err.message, 'good fail')
+                showAndThrow(y, undefined);
+                if(!parallel.notAsSoonAsPosible){
+                    showAndThrow(target.join(''), 'tar');
+                }
+            }
+        })
+    });
+})
